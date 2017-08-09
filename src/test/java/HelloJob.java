6479924213f3cb58ea5a1677b3f758c675e4740d
nwj <zjywnwj@tom.com>
2017-08-09 23:43:57 +0800

    增加定时任务案例

---
+++ src/test/java/HelloJob.java
@@ -0,0 +1,12 @@
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class HelloJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("job doing something...");
+
+
+    }
+}
